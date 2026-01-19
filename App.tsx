acd24b4e659f843a2cb8ca5a59261092274ea584
Furkan Yasin Engin <furkanyasinengin@gmail.com>
2026-01-19 18:47:12 +0300

    feat: implement core qr rendering engine using skia

--- App.tsx
+++ App.tsx
@@ -1,28 +1,41 @@
-
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import {
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Dimensions } from 'react-native';
 
 import './global.css';
-import { Canvas } from '@shopify/react-native-skia';
+import { Canvas, Rect, Group } from '@shopify/react-native-skia';
+import { useEffect, useState } from 'react';
+import { getQRCode } from './src/utils/qrGenerator';
 
 function App() {
-
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={'light-content' } />
+      <StatusBar barStyle={'light-content'} />
       <AppContent />
     </SafeAreaProvider>
   );
 }
 
 function AppContent() {
+  const [url, setUrl] = useState<string>('github.com');
+  const [qrSize, setQrSize] = useState<number>(25);
+  const [qrData, setQrData] = useState<Uint8Array | []>([]);
+  const [qrSquareSize, setQrSquareSize] = useState<number>(0);
+
+  const { width } = Dimensions.get('window');
+
+  // const qrObject = getQRCode(url);
+  // const { size, data } = qrObject.modules;
+  // const squareSize = (width - 50) / size;
+
+  useEffect(() => {
+    if (!url) return;
+    const qrObject = getQRCode(url);
+    const { size, data } = qrObject.modules;
+    setQrSize(size);
+    setQrData(data);
+    setQrSquareSize((width - 50) / size);
+  }, [url, width]);
 
   return (
     <View
@@ -30,7 +43,25 @@ function AppContent() {
       style={styles.container}
     >
       <Text>QR Code Generator</Text>
-      <Canvas style={{ width: 200, height: 200 }} />
+      <Text>{url}</Text>
+
+      <View className="bg-red-500">
+        <Canvas style={{ width: width - 50, height: width - 50 }}>
+          {Array.from(qrData).map((value, index) => {
+            if (value)
+              return (
+                <Rect
+                  key={index}
+                  x={(index % qrSize) * qrSquareSize}
+                  y={Math.floor(index / qrSize) * qrSquareSize}
+                  width={qrSquareSize}
+                  height={qrSquareSize}
+                  color="black"
+                />
+              );
+          })}
+        </Canvas>
+      </View>
     </View>
   );
 }
---
+++ src/utils/qrGenerator.ts
@@ -0,0 +1,40 @@
+import QRCode from "qrcode";
+import type { QRCode as QRCodeType } from "qrcode";
+
+export const getQRCode = (text: string): QRCodeType => {
+  return QRCode.create(text, { errorCorrectionLevel: "H" });
+};
+
+export const isFinderPattern = (
+  row: number,
+  col: number,
+  size: number
+): boolean => {
+  if (
+    (row < 7 && col < 7) ||
+    (row < 7 && col >= size - 7) ||
+    (row >= size - 7 && col < 7)
+  )
+    return true;
+  return false;
+};
+
+export const isLogoArea = (row: number, col: number, size: number): boolean => {
+  const logoRatio = 0.2;
+  const logoSize = size * logoRatio;
+  const padding = 1.5;
+  const start = (size - logoSize) / 2 - padding;
+  const end = size - start;
+  if (row >= start && row <= end && col >= start && col <= end) return true;
+
+  return false;
+};
+
+// export const downloadCanvasAsPng = (canvas: HTMLCanvasElement) => {
+//   const data = canvas.toDataURL("image/png");
+//   const downloadElement = document.createElement("a");
+//   downloadElement.href = data;
+//   downloadElement.download = "qr-code.png";
+//   downloadElement.click();
+//   downloadElement.remove();
+// };

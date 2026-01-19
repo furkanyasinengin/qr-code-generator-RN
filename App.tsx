3c16e88ba4cd0944a9ea9bbd163288ea1a196899
Furkan Yasin Engin <furkanyasinengin@gmail.com>
2026-01-19 16:18:21 +0300

    chore: init project with react native cli, nativewind and skia setup

--- App.tsx
+++ App.tsx
@@ -1,37 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
+import './global.css';
+import { Canvas } from '@shopify/react-native-skia';
+
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={'light-content' } />
       <AppContent />
     </SafeAreaProvider>
   );
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View
+      className="flex-1 bg-gray-500 items-center justify-center"
+      style={styles.container}
+    >
+      <Text>QR Code Generator</Text>
+      <Canvas style={{ width: 200, height: 200 }} />
     </View>
   );
 }
